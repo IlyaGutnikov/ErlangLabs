8293b4435d2aa49f70d39c716b0ad3e96a7d73ce
Илья <ilyagutnikov93@gmail.com>
2016-02-17 01:33:00 +0300

    Сделал первое задание из двух.

--- lab1/fib.erl
+++ lab1/fib.erl
@@ -1,5 +1,6 @@
 -module(fib).
 -export([fib_p/1, fib_g/1, tail_fib/1]).
+-export([test_fib_p/1, test_tail_fib/0]).
 
 %% non tail
 fib_p(0) -> 0;
@@ -7,7 +8,7 @@ fib_p(1) -> 1;
 fib_p(N) -> fib_p(N-1) + fib_p(N-2).
 
 %% with guard (when) 
-fib_g(N) when N == 0 -> 0 ;
+fib_g(N) when N == 0 -> 0;
 fib_g(N) when N == 1 -> 1;
 fib_g(N) -> fib_g(N-1) + fib_g(N-2).
 
@@ -19,3 +20,7 @@ fib_t(EndCond, 0, []) -> fib_t(EndCond, 1, [1]);
 fib_t(EndCond, 1, [1]) -> fib_t(EndCond, 2, [1, 1]);
 fib_t(EndCond, Counter, [X,Y|Tail]) when EndCond =/= Counter -> fib_t(EndCond, Counter + 1, [X + Y, X, Y|Tail]);
 fib_t(N, N, [Head| _]) -> Head.
+
+test_fib_p(M) -> timer:tc(fib,fib_p, [M]).
+
+test_tail_fib() -> timer:tc(fib,tail_fib, [10000]).
