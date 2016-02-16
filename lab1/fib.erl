ca54f26c5f559888dc355f797d0cc54de420aac8
Илья <ilyagutnikov93@gmail.com>
2016-02-17 00:46:08 +0300

    Сделал хвостовую рекурсию.

--- lab1/fib.erl
+++ lab1/fib.erl
@@ -1,10 +1,21 @@
 -module(fib).
--export([fib_p/1, fib_g/1]).
+-export([fib_p/1, fib_g/1, tail_fib/1]).
 
+%% non tail
 fib_p(0) -> 0;
 fib_p(1) -> 1;
 fib_p(N) -> fib_p(N-1) + fib_p(N-2).
 
+%% with guard (when) 
 fib_g(N) when N == 0 -> 0 ;
 fib_g(N) when N == 1 -> 1;
 fib_g(N) -> fib_g(N-1) + fib_g(N-2).
+
+%% with tail
+tail_fib(N) -> fib_t(N, 0, []).
+fib_t(1, _, _) -> 1;
+fib_t(2, _, _) -> 1;
+fib_t(EndCond, 0, []) -> fib_t(EndCond, 1, [1]);
+fib_t(EndCond, 1, [1]) -> fib_t(EndCond, 2, [1, 1]);
+fib_t(EndCond, Counter, [X,Y|Tail]) when EndCond =/= Counter -> fib_t(EndCond, Counter + 1, [X + Y, X, Y|Tail]);
+fib_t(N, N, [Head| _]) -> Head.
