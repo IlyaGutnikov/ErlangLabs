d1764b07a3b43e0ee8f068f972638835298c787c
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-03-04 15:57:17 +0300

    Сделал лабораторную работу №2.

--- lab2/proc_sieve.erl
+++ lab2/proc_sieve.erl
@@ -1,5 +1,7 @@
 -module(proc_sieve).
 -export([generate/1]).
+-export([sieve_sub_func/2]).
+-export([gen_print/1]).
 
 generate(MaxN) ->
         Pid = sieve(),
@@ -19,6 +21,26 @@ sieve() ->
 	%%Модуль, функция, аргументы
     spawn(proc_sieve, sieve_sub_func, [0, void]).
 
+sieve_sub_func(0, InvalidPid) ->
+    receive 
+        P -> sieve_sub_func(P, InvalidPid)
+    end; 
+
+sieve_sub_func(P, NextPid) when is_pid(NextPid) ->
+    receive 
+        {done, From} ->
+            NextPid ! {done, self()},
+            receive 
+                LstOfRes -> 
+                    From ! [P] ++ LstOfRes 
+            end;
+        N when N rem P == 0 -> 
+            sieve_sub_func(P, NextPid);
+        N when N rem P /= 0 -> 
+            NextPid ! N,
+            sieve_sub_func(P, NextPid)
+    end;
+
 sieve_sub_func(P, Invalid) ->
     receive 
         {done, From} ->
@@ -30,3 +52,7 @@ sieve_sub_func(P, Invalid) ->
             Pid ! N,
             sieve_sub_func(P, Pid)
     end.
+
+gen_print(MaxN) -> lists:foreach(
+	fun(X)->io:format("~w ",[X]) end,
+	generate(MaxN)).
