54c192b9428c0873de6ae54382d7673531f71106
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-03-25 16:57:48 +0300

    Сделал первую функцию.

--- lab3/rss_parse.erl
+++ lab3/rss_parse.erl
@@ -1,1 +1,11 @@
 -module(rss_parse).
+
+-export([is_rss2_feed/1]).
+
+is_rss2_feed(F) ->
+    {XML, _} = xmerl_scan:file(F),
+    is_rss2_feed_sub_func(XML).
+
+is_rss2_feed_sub_func(XML) ->
+    Ver = xmerl_xpath:string("/rss[@version='2.0']/text()", XML),
+    length(Ver) =/= 0. % если нашло хоть один version=2.0, то возвращаем true
